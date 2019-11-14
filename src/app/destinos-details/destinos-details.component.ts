290d5c5acee73b420180590fd4369eaaa783da60
Valeria Trotta <valeria.trotta@correo.unimet.edu.ve>
2019-11-13 23:15:14 -0400

    Create destinos-details.component.ts

---
+++ src/app/destinos-details/destinos-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-destinos-details',
+  templateUrl: './destinos-details.component.html',
+  styleUrls: ['./destinos-details.component.scss']
+})
+export class DestinosDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
