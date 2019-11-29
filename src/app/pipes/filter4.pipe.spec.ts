4b52e83676a68af81b23163210ed5e6506dd083b
Luis Arturo Castaneda <luis.castaneda@correo.unimet.edu.ve>
2019-11-28 21:14:29 -0400

    tutu

---
+++ src/app/pipes/filter4.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { Filter4Pipe } from './filter4.pipe';
+
+describe('Filter4Pipe', () => {
+  it('create an instance', () => {
+    const pipe = new Filter4Pipe();
+    expect(pipe).toBeTruthy();
+  });
+});
