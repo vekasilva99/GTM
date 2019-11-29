ffac8aa3eee14359ca86fb563edcc44d55495a3a
Valeria Trotta <valeria.trotta@correo.unimet.edu.ve>
2019-11-29 14:59:06 -0400

    Update lista-estados.component.ts

--- src/app/lista-estados/lista-estados.component.ts
+++ src/app/lista-estados/lista-estados.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {estado} from '../../app/estados/estado';
-import {ESTADOS} from  '../../app/estados/mock-estados';
+import {ESTADOS} from '../../app/estados/mock-estados';
 import {EstadoService} from '../Services/estado.service';
 import {ListaEstadosService} from '../Services/lista-estados.service';
 
@@ -12,31 +12,7 @@ import {ListaEstadosService} from '../Services/lista-estados.service';
 })
 export class ListaEstadosComponent implements OnInit {
 
-  estados: estado[]=[];
-  loading:boolean=false;
-  
-  constructor(private estadoService: EstadoService, public listaEstadosService: ListaEstadosService) { }
-
   ngOnInit() {
-    // this.getEstados();
-    this.loading=false;
-    this.estadoService.getOrders().subscribe(array=>{
-      array.map(item => {
-        const estado:estado={
-          id:item.payload.doc.id,
-          ...item.payload.doc.data()
-        }
 
-        this.estados.push(estado);
-        
-      });
-      this.loading=false;
-    });
-    
-  }
-
-  getEstados(){
-    this.estadoService.getEstados().subscribe(estado => (this.estados=estado));
-    }
-
+}
 }
